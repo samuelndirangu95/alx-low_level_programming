5f44ab1b1968036073f020ca1c8ce1c7b52da408
samuelndirangu95 <samuelndirangu95@gmail.com>
2023-06-15 20:13:00 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+
+/**
+ *
+ * main - Entry point of the program
+ *
+ * return 0 -Indicates successful execution.
+ *
+ */
+int main()
+{
+	puts("Programming is like pbuilding a multilingual puzzle"):
+	
+	return (0);
+
+}
