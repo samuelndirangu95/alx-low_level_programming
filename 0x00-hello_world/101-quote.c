c9e30403a1e8d85443b4e1e454f63e5db7b09c64
samuelndirangu95 <samuelndirangu95@gmail.com>
2023-06-18 09:08:45 -0700

    Added program to print to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
 #include <unistd.h>
 
 /**
- * main - print "and that piece of art is useful" 
- * - Dora Korpar, 2015-10-19"
- *   followed by new line to Stderror
+ *main - print and that piece of art is useful"
+ *	- Dora Korpar, 2015-10-19
+ *	followed by new line to Stderror
  * Return: 1 to indicate an error condition
  */
 
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-	
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }
