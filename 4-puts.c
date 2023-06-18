bf250ae2e02d751a4a64f650615d324151f4402f
samuelndirangu95 <samuelndirangu95@gmail.com>
2023-06-18 02:56:29 -0700

    Added file 4-puts

---
+++ 4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return:0 if project is successful, non-zero otherwise
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
