fb8920b9bf02fc4f4ab549a1f4521cf2c98a3492
samuelndirangu95 <samuelndirangu95@gmail.com>
2023-06-18 04:02:52 -0700

    Added file 5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-prints  phrase with proper grammar, but the outcome is a piece of art,
+ * Return: 0 to show function is successful, nonzero ig otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
