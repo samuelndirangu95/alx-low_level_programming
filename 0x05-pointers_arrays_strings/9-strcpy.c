f7576ac5cdd351379473afda4f8e61fd82c93b1c
samuelndirangu95 <samuelndirangu95@gmail.com>
2023-06-28 05:11:00 +0000

    9. strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string including '\0' to buffer
+ * @dest: buffer where string is copied to
+ * @src: string being copied
+ * Return: return dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int length;
+
+	for (length = 0; src[length] != '\0'; length++)
+	{
+	}
+
+	for (i = 0; i < length; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
